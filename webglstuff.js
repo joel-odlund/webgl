d65274e0f7a2166ce07fc8470f72637d2eb3e1b3
Joel Ödlund <joel.odlund@seal-software.com>
2017-10-25 18:54:21 +0200

    more stuff

--- webglstuff.js
+++ webglstuff.js
@@ -31,6 +31,33 @@ function createShader(gl, type, source){
 }
 function resizeCanvas(canvas){
 
-    canvas.height = 300;
-    canvas.width = 300;
+    canvas.height = 500;
+    canvas.width = 600;
 }
+
+function randomInt(range) {
+    return Math.floor(Math.random() * range);
+}
+
+// Fills the buffer with the values that define a rectangle.
+
+function setRectangle(gl, x, y, width, height) {
+    var x1 = x;
+    var x2 = x + width;
+    var y1 = y;
+    var y2 = y + height;
+    
+    // NOTE: gl.bufferData(gl.ARRAY_BUFFER, ...) will affect
+    // whatever buffer is bound to the `ARRAY_BUFFER` bind point
+    // but so far we only have one buffer. If we had more than one
+    // buffer we'd want to bind that buffer to `ARRAY_BUFFER` first.
+    
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
+        x1, y1,
+        x2, y1,
+        x1, y2,
+        x1, y2,
+        x2, y1,
+        x2, y2]), gl.STATIC_DRAW);
+}
+
